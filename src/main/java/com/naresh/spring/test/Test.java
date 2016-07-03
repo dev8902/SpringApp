f3ddb87047914b9e49003cc905393f2f3f7268bf
Naresh <naresh>
2016-07-03 20:36:16 +0530

    test

--- src/main/java/com/naresh/spring/test/Test.java
+++ src/main/java/com/naresh/spring/test/Test.java
@@ -3,10 +3,26 @@
  */
 package com.naresh.spring.test;
 
+import java.nio.ByteBuffer;
+
 /**
  * @author Naresh
  *
  */
 public class Test {
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+	public static void main(String[] args) {
+		ByteBuffer byteBuffer=ByteBuffer.allocate(8);
+		byte b2=10;
+		byteBuffer.put(b2);
+		byteBuffer.put(b2);
+		byte[] b=byteBuffer.array();
+		for(byte b1:b){
+			System.out.println(b1);
+		}
+		
+	}
 
 }
